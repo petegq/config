34b613d6c2746085bf046843ea3bc80113abd2d7
Peter Garside-Quin <petegq@gmail.com>
2022-01-04 11:20:49 +0000

    Add prettierrc variants

---
+++ prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	semi: false,
+	singleQuote: true,
+ 	jsxSingleQuote: true,
+	useTabs: true,
+	tabWidth: 4,
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	arrowParens: 'avoid',
+	trailingComma: 'all',
+	endOfLine: 'lf',
+};
