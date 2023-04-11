68c5b010db0085b97ee80204f77bfd7453c483fb
Peter Garside-Quin <petegq@gmail.com>
2023-04-11 12:28:52 +0100

    Comment and tidy

--- make-project.sh
+++ make-project.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
-# cp make-project.sh /usr/local/bin/make-project.sh
-# chmod u+x /usr/local/bin/make-project.sh
-# echo alias proj="bash /usr/local/bin/make-project.sh" >> ~/.zshrc
+:'
+To run from the command line:
+chmod u+x ./make-project.sh # (or wherever you want to put it)
+./make-project.sh <project-name> "<package> <package> <package> ..."
+'
+
+:'
+Example NPM script to run:
+Add "makenode": "./make-project.sh $npm_config_dirname $npm_config_pkgs" to package.json
+npm run makenode --dirname="testDIR" --pkgs="axios mocha"
+'
 
 mkdir $1
 cd $1
 touch index.js
 npm init -y
+
+# Add pased in packages
 echo 'node_modules' >>./.gitignore
 npm i $2
+
+# Add prettier, pull in the config files and format script
+npm i prettier
 wget https://raw.githubusercontent.com/petegq/config/master/.prettierignore
 wget https://raw.githubusercontent.com/petegq/config/master/.prettierrc
-npm i prettier nodemon
-npm pkg set scripts.dev='nodemon index.js --port 8000'
 npm pkg set scripts.format='prettier --write "**/*.{js,jsx,ts,tsx}"'
+
+# Add nodemon, dev and kill scripts
+npm i nodemon
+npm pkg set scripts.dev='nodemon index.js --port 8000'
 npm pkg set scripts.kill='lsof -ti tcp:8000 | xargs kill'
+
+# Initialize git and open in VS Code
 git init
 git add .
 git commit -m 'Initial commit'
