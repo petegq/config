08f349c3961444c0bfa0839e68ba8ff08071a9d6
Peter Garside-Quin <petegq@gmail.com>
2023-04-11 11:44:34 +0100

    Moving makenode to bash script, less unweildy

--- make-project.sh
+++ make-project.sh
@@ -4,12 +4,16 @@
 # chmod u+x /usr/local/bin/make-project.sh
 # echo alias proj="bash /usr/local/bin/make-project.sh" >> ~/.zshrc
 
-cd
-
-mkdir ~/projects/$1
-
-cd ~/projects/$1
-
+mkdir $1
+cd $1
+touch index.js
 npm init -y
-
+echo 'node_modules' >>./.gitignore
+npm i $2
+wget https://raw.githubusercontent.com/petegq/config/master/.prettierignore
+wget https://raw.githubusercontent.com/petegq/config/master/.prettierrc
+npm i prettier
+git init
+git add .
+git commit -m 'Initial commit'
 code .
