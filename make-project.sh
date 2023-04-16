967aac6a144a6ce07e4a0c01b3e966ba7b4ee065
Peter Garside-Quin <petegq@gmail.com>
2023-04-17 00:30:43 +0100

    Tidy

--- make-project.sh
+++ make-project.sh
@@ -12,28 +12,36 @@ Add "makenode": "./make-project.sh $npm_config_dirname $npm_config_pkgs" to pack
 npm run makenode --dirname="testDIR" --pkgs="axios mocha"
 '
 
-mkdir $1
-cd $1
-touch index.js
-npm init -y
+function makeNode {
+    mkdir $1
+    cd $1
+    touch index.js
+    npm init -y
 
-# Add pased in packages
-echo 'node_modules' >>./.gitignore
-npm i $2
+    # Add pased in packages
+    echo 'node_modules' >>./.gitignore
+    npm i $2
 
-# Add prettier, pull in the config files and format script
-npm i prettier
-wget https://raw.githubusercontent.com/petegq/config/master/.prettierignore
-wget https://raw.githubusercontent.com/petegq/config/master/.prettierrc
-npm pkg set scripts.format='prettier --write "**/*.{js,jsx,ts,tsx}"'
+    # Add prettier, pull in the config files and format script
+    npm i prettier
+    wget https://raw.githubusercontent.com/petegq/config/master/.prettierignore
+    wget https://raw.githubusercontent.com/petegq/config/master/.prettierrc
+    npm pkg set scripts.format='prettier --write "**/*.{js,jsx,ts,tsx}"'
 
-# Add nodemon, dev and kill scripts
-npm i nodemon
-npm pkg set scripts.dev='nodemon index.js --port 8000'
-npm pkg set scripts.kill='lsof -ti tcp:8000 | xargs kill'
+    # Add nodemon, dev and kill scripts
+    npm i nodemon
+    npm pkg set scripts.dev='nodemon index.js --port 8000'
+    npm pkg set scripts.kill='lsof -ti tcp:8000 | xargs kill'
 
-# Initialize git and open in VS Code
-git init
-git add .
-git commit -m 'Initial commit'
-code .
+    # Initialize git and open in VS Code
+    git init
+    git add .
+    git commit -m 'Initial commit'
+    code .
+}
+
+if [ -d "directory_name" ]; then
+    echo "Directory already exists in the current working directory."
+else
+    makenode
+fi
